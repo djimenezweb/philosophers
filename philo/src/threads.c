64cb5b63024a1203a1cd0ca0e3b36088c4938ff7
Daniel Jiménez <danielji@student.42madrid.com>
2025-08-22 11:30:03 +0200

    Reverted created threads to single philosopher

--- philo/src/threads.c
+++ philo/src/threads.c
@@ -6,32 +6,41 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 09:44:37 by danielji          #+#    #+#             */
-/*   Updated: 2025/08/22 11:02:49 by danielji         ###   ########.fr       */
+/*   Updated: 2025/08/22 11:18:56 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "philosophers.h"
 
-void	*routine(void *arg)
+/* void	*routine(void *arg)
 {
 	t_config	*c;
 
-	c = (t_config *) arg;
+	c = (t_config *)arg;
 	timestamp(1, "says hello");
+	return (NULL);
+} */
+
+void	*routine(void *arg)
+{
+	t_philo	*p;
+
+	p = (t_philo *)arg;
+	timestamp(p->id, "says hello");
 	return (NULL);
 }
 
 void	create_threads(t_config config)
 {
 	int			i;
-	pthread_t	id;
+	pthread_t	thread;
 
 	i = 0;
 	while (i < config.number)
 	{
-		config.philo_array[i].thread = id;
-		pthread_create(&id, NULL, routine, (void *)&config);
-		pthread_join(id, NULL);
+		config.philo_array[i].thread = thread;
+		pthread_create(&thread, NULL, routine, (void *)&config.philo_array[i]);
+		pthread_join(thread, NULL);
 		i++;
 	}
 }
